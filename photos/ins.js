7793cd44e2c2c1cd1a5775b9dcafa4fc9ee9b01e
nemo158 <chunshuyulesa@126.com>
2020-08-03 15:46:55 +0800

    Site updated: 2020-08-03 15:45:41

--- photos/ins.js
+++ photos/ins.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\nemo-space\assets\css\APlayer.min.css"><script src="\nemo-space\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>
 /******/
 (function(modules) { // webpackBootstrap
     /******/ // The module cache
